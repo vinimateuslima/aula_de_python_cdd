d1de41447acdccb50117dcc4ecdbef4c9dd70dcf
vinimateuslima <viniciusmateus.devm@gmail.com>
2024-10-09 14:28:08 -0300

    atividade de sistema de login

--- Atividades/sistema_de_login.py
+++ Atividades/sistema_de_login.py
@@ -1,4 +1,4 @@
-usuario = [0]*5
+usuario = [""]*5
 
 tam = len(usuario)
 
@@ -11,18 +11,63 @@ opcao = int(input("1 - Cadastro \n"
           " 3 - Login \n"
           " 4 - Sair \n "
           "Selecione uma opção: "))
-
+print()
 while opcao != 4:
 
 
     if opcao == 1:
-        for i in range(10):
-            print(i)
-            if i == 5:
+        for i in range(tam):
+            if usuario[i] == "":
+                usuario[i] = input("Digite um usuário: ")
+                senha[i] = input("Digite a senha do usuário: ")
+                print("Cadastro realizado com sucesso!")
                 break
 
+        else:
+            print()
+            print("Limite de usuários excedido!")
+
+    if opcao == 2:
+        print("Lista de usuários cadastrados")
+        print(usuario)
+        print(senha)
+
+    if opcao == 3:
+        print("-=-=-=-=-=-=-=-= Realizar Login =-=-=-=-=-=-=-=-=-")
+
+        usuarioDigitado = input("Digite o usuário: ")
+
+        for i in range(tam):
+
+            if  usuarioDigitado == usuario[i] :
+                tentativa = 2
+
+                while tentativa >= 0:
+
+                    senhaDigitada = input("Digite a senha: ")
+
+                    if senha[i] == senhaDigitada:
+                        print(f"Login realizado com sucesso! Bem vindo(a) {usuario[i]}")
+                        break
+                    else:
+                        print(f"Senha incorreta! Você possui {tentativa} tentativas.")
+                        tentativa -= 1
+                else:
+                    print("Número de tentativas excedidas!")
+                    break
+
+
+
+
+
+
+
+    print()
+    print("Deseja realizar outra operação?")
     opcao = int(input("1 - Cadastro \n"
               " 2 - Mostrar \n"
               " 3 - Login \n"
               " 4 - Sair \n "
               "Selecione uma opção: "))
+else:
+    print("Saindo do programa...")
