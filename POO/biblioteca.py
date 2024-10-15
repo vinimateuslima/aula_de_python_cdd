e0acb21289f294a826a271f3537bab6d137d90ce
vinimateuslima <viniciusmateus.devm@gmail.com>
2024-10-15 16:54:44 -0300

    aula 15-10

--- POO/biblioteca.py
+++ POO/biblioteca.py
@@ -1,14 +1,29 @@
 from operator import truediv
 
-
 class Pessoa:
-    def __init__(self, nome, peso, idade, andando=False):
+    def __init__(self, nome, peso, idade, andando=False, comendo=False, dormindo=False):
         self.nome = nome
         self.peso = peso
         self.idade = idade
         self.andando = andando
+        self.comendo = comendo
+        self.dormindo = dormindo
+
+    def comer(self):
+        if self.comendo==True:
+            print("Ja estou comendo")
+        elif self.andando==True:
+            print("Não posso comer, estou andando")
+        elif self.dormindo==True:
+            print("Não posso comer, estou dormindo")
+        else:
+            print("Vou comer")
+            self.comendo = True
+
+
 
     def andar(self):
+
         if self.andando == False:
             print(f"{self.nome} Está andando")
             self.andando = True
@@ -23,9 +38,37 @@ class Pessoa:
        else:
            print("Ja estou parado")
 
-    def comer(self):
-        print(f"{self.nome} foi comer")
+
 
     def dormir(self):
         print(f"{self.nome} foi dormir")
+
+
+class Animal:
+    def __init__(self, nome, cor, comendo=False):
+        self.nome = nome
+        self.cor = cor
+        self.comendo = comendo
+
+        def comer (self):
+            print("Estou comendo")
+
+class Gato(Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
+
+    def miar (self):
+        print(f" O {self.nome} foi miando...")
+
+class Cachorro(Animal):
+    def __init__(self, nome, cor):
+        super().__init__(nome, cor)
+
+    def latir(self):
+        print(f" O {self.nome} está latindo...")
+
+    def comer(self):
+        print(f" O cachorro {self.nome} foi comer...")
+
+
 
--- POO/main.py
+++ POO/main.py
@@ -3,4 +3,14 @@ from biblioteca import *
 p1 = Pessoa("João", 75, 21)
 p2 = Pessoa("Luiza", 60, 19)
 
-p1.pararDeAndar()
+p1.dormir()
+p1.comer()
+
+g1 = Gato("ProfessorWellington", "laranja")
+
+g1.miar()
+
+c1 = Cachorro("Spock", "marron")
+
+c1.latir()
+c1.comer()
